2cac0f7a09f9dba162777cd160ec8aa10ced8c83
khai <khai.buivinh3005@gmail.com>
2023-04-01 12:33:55 +0700

    setup environment

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "../public/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="text-center">hello</div>
+    </div>
+  );
+}
+
+export default App;
